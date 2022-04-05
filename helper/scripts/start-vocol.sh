1f41a339e4ea50cb1408e0135d25eb90b4dc8b08
Ashish Pal <61803215+ashishp98@users.noreply.github.com>
2022-04-05 13:50:20 +0530

    Update start-vocol.sh

--- helper/scripts/start-vocol.sh
+++ helper/scripts/start-vocol.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
 # Run fuseki
 echo "RUNNING FUSEKI"
 cd /home/project/vocol/helper/tools/apache-jena-fuseki
 sh ../../scripts/run.sh
-
 # Run Vocol
 echo "RUNNING VOCOL"
 cd /home/project/vocol
