e3d75766ee7454b2d7809f249a586702354eea51
Ashish Pal <61803215+ashishp98@users.noreply.github.com>
2022-04-05 14:05:23 +0530

    Update start-vocol.sh

--- helper/scripts/start-vocol.sh
+++ helper/scripts/start-vocol.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# Starting script for Vocol and Fuseki in the ZF Demonstrator container
+# By Fraunhofer IAIS-EIS, July 2019
+
 # Run fuseki
 echo "RUNNING FUSEKI"
 cd /home/project/vocol/helper/tools/apache-jena-fuseki
 sh ../../scripts/run.sh
+
 # Run Vocol
 echo "RUNNING VOCOL"
 cd /home/project/vocol
