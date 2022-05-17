9c90ef965f31f300edbebc3fbd0cab38d1a0b0a4
Ashish Pal <61803215+ashishp98@users.noreply.github.com>
2022-05-17 20:45:37 +0530

    Update forceStratup.js

--- routes/forceStratup.js
+++ routes/forceStratup.js
@@ -171,7 +171,7 @@ fs.exists(path, function (exists) {
         // update queries in fuseki //
         /////////////////////////////
         // update fuseki queries file with some user-defined queries if there is any
-        var fusekiQueriesFilePath = 'webapp/js/app/qonsole-config.js';
+        var fusekiQueriesFilePath = 'webapp/js/app/qonsole/qonsole-config.js';
         // read contents of the file with the filePathgetTree
         var fusekiQuerieFileContent = fs.readFileSync(
           fusekiQueriesFilePath, 'utf8');
